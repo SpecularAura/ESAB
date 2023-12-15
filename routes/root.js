5eda1b61bed880b6d8e8879d4c715ba3ec346865
Aum Kulkarni <aumwinay@gmail.com>
2023-12-16 01:06:07 +0530

    Restructured according to Shivams suggestions

--- routes/root.js
+++ routes/root.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/authController");
+const logoutController = require("../controllers/logoutController");
+const registerController = require("../controllers/registerController");
 
 router.get("", (req, res) => {
   res.sendStatus(200);
 });
+
+router.post("/login", authController.handleLogin);
+router.get("/logout", logoutController.handleLogout);
+router.post("/register", registerController.handleNewUser);
 
 module.exports = router;
